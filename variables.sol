349a09f6f1d17f96f78b12474bcad79fcd2c544b
Shahriar Hossain <shahriar_cse@live.com>
2018-04-22 21:30:38 +0600

    declaring variable in solidity

---
+++ variables.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.18;
+
+contract variables{
+    string public someName="Shahriar";
+    int public someRandomNumber =-2342813;
+    uint public somePositiveRandomNumber =23491;
+    address public myWalletAddress = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+}
