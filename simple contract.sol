276551e7fe085b8f66287a056690f82ac1566021
Shahriar Hossain <shahriar_cse@live.com>
2018-04-22 11:03:30 +0600

    The Basics

--- simple contract.sol
+++ simple contract.sol
@@ -1,15 +1,26 @@
 pragma solidity ^0.4.18;
 
 contract Employee{
-    string public name = 'Shahriar';
-    uint age= 26;
+    string public name = "Shahriar";
+    uint private age= 26;
+    string bloodGroup;
+    uint public initialBalance =0;
     
+    //Function visibility is not specified. Default is 'public'
+    constructor(){
+        initialBalance = 10;
+    }
+
     function getName() public constant returns(string){
         return (name);
     }
     
-    //Function visibility is not specified. Default is 'public'
-    function setName(string _name) {
+    function getEmployeeInfo() public constant returns(string, uint, string, uint)
+    {
+        return (name, age, bloodGroup, initialBalance);
+    }
+
+    function setName(string _name) public {
         name = _name;
     }
 }
